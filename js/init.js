316a83465ebe92316a5f800f78fd31c5a5c30e3f
chatmacjavatar <118499808+chatmacjavatar@users.noreply.github.com>
2022-11-17 22:27:46 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDdapj3H3Dd2w9DHpLHwbzwpwSwU7rItpg",
-    authDomain: "chatrickfb.firebaseapp.com",
-    projectId: "chatrickfb",
-    storageBucket: "chatrickfb.appspot.com",
-    messagingSenderId: "815561540833",
-    appId: "1:815561540833:web:2cf0f52c625e8e281723a0",
-    measurementId: "G-5W02XQR53J"
+    apiKey: "AIzaSyAokL475ehDSHAisIr-eksij8I_zyRbEpc",
+  authDomain: "chatmacjavatar5.firebaseapp.com",
+  projectId: "chatmacjavatar5",
+  storageBucket: "chatmacjavatar5.appspot.com",
+  messagingSenderId: "41607495724",
+  appId: "1:41607495724:web:7faf8f7937d41640901189",
+  measurementId: "G-ZJXG5XPG3R"
     });
